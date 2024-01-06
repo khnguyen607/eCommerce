7a7251ea56e42bc2288f5278c53bd796ea2cb76f
khnguyen.607 <khnguyen.job@gmail.com>
2024-01-06 10:40:32 +0700

    update more..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuickViewModalComponent } from './components/quick-view-modal/quick-view-modal.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    QuickViewModalComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
